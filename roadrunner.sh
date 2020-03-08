12fbb46a5e0189ac13fc456d0044ae292ce2523c
Bing Rao <bing.rao@outlook.com>
2020-03-07 23:27:15 -0500

    roadrunner scripts

---
+++ roadrunner.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+RR_PROJECT_PATH=/home/bing/workspace/RoadRunner
+RR_BUILD_PATH=${RR_PROJECT_PATH}/build
+
+echo "Compiling whole project ..."
+cd $RR_PROJECT_PATH
+ant
+cd -
+
+if ! [ -x "$(command -v rrrun)" ]
+then
+  echo 'Error: rrrun command is not added to Path'
+  source ${RR_PROJECT_PATH}/msetup
+fi
+
+##################################################################
+
+APP_HOME=`pwd`
+APP_JAR=${APP_HOME}/jar/test.jar
+APP_CLASS="test.Test"
+APP_ARGS=
+RR_TOOL="PE"
+RR_ARGS=" -classpath=${APP_JAR} -tool=${RR_TOOL} -maxTid=$[AVAIL_PROCS + 10]"
+
+LOG_FILE=${APP_HOME}/log/log.txt
+if [ ! $# -eq 0 ]
+then
+  RR_TOOL=$1
+  APP_CLASS=$2
+fi
+
+
+if [ ! -f ${LOG_FILE} ]
+then
+  echo "Log file does not exist and create it ..."
+  mkdir -p ${APP_HOME}/log
+  touch ${LOG_FILE}
+fi
+echo "Using RoadRunner to extract dynamc data trace for Java Thread ..."
+echo "Using Specify RoadRunner Configuration: $RR_ARGS"
+echo "App jar: ${APP_JAR}"
+echo "App main class: ${APP_CLASS}"
+echo "App arguments: ${APP_ARGS}"
+echo "Log file: ${LOG_FILE}"
+
+
+set -x
+rrrun ${RR_ARGS} ${APP_CLASS} ${APP_ARGS} &> ${LOG_FILE}
+
